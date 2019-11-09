3ac084499cbec92fd2081b076ed42fe657cb1e0b
Mihai Georgevici <mihai@titlecapture.com>
2019-11-09 21:08:36 +0200

    Initial Commit

---
+++ CarCheckUp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarCheckUp
+//
+//  Created by MacBook Air on 11/9/19.
+//  Copyright © 2019 ArteneR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
